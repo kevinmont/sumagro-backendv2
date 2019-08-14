e5659317ff3a723e548e7e93abd7ca4329c20737
cheko0 <srq.wdap@gmail.com>
2019-08-14 14:24:34 -0500

    Se realizo la páginación

--- functions/src/controller/ingenioController.ts
+++ functions/src/controller/ingenioController.ts
@@ -1,10 +1,10 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 import IngenioDao from '../dao/ingenioDao';
 import AddressDao from '../dao/addressDao';
 import OrderDao from '../dao/orderDao';
 import SubOrdersDao from '../dao/subOrdersDao';
-import {Ingenio} from '../models/ingenio';
-import {Address} from '../models/address';
+import { Ingenio } from '../models/ingenio';
+import { Address } from '../models/address';
 import PdfHelper from '../utils/Pdf-Helper';
 import { Nodemailers } from '../utils/Nodemailer-helper';
 import Config from '../models/config';
@@ -13,51 +13,51 @@ import * as log4js from 'log4js';
 const logger = log4js.getLogger();
 logger.level = 'debug';
 
-export default class IngenioController{
-    public ingenioDao : IngenioDao;
+export default class IngenioController {
+    public ingenioDao: IngenioDao;
     public addressDao: AddressDao;
     public orderDao: OrderDao;
-    public subOrdersDao : SubOrdersDao;
+    public subOrdersDao: SubOrdersDao;
     public pdfHelper: PdfHelper;
-    public nodemailerHelper: Nodemailers; 
-    public config:any;
-    constructor(){
+    public nodemailerHelper: Nodemailers;
+    public config: any;
+    constructor() {
         this.config = Config;
         this.ingenioDao = new IngenioDao();
         this.addressDao = new AddressDao();
         this.orderDao = new OrderDao();
         this.subOrdersDao = new SubOrdersDao();
         this.pdfHelper = new PdfHelper();
-        this.nodemailerHelper = new Nodemailers(this.config); 
-    } 
+        this.nodemailerHelper = new Nodemailers(this.config);
+    }
 
-    async createeIngenio(req:Request, res:Response){
+    async createeIngenio(req: Request, res: Response) {
         logger.info('CONTROLLER: method createeIngenio Starting');
-        if(!req.body.email) throw res.status(400).send("email is required")
-        if(!req.body.address) throw res.status(400).send("address is required")
-        if(!req.body.name) throw res.status(400).send("name is required")
-        if(!req.body.address.street) throw res.status(400).send("Street is required")
-        if(!req.body.address.number) throw res.status(400).send("number is required")
-        if(!req.body.address.city) throw res.status(400).send("city is required")
-        if(!req.body.address.location) throw res.status(400).send("location is required")
-        if(!req.body.address.municipality) throw res.status(400).send("municipality is required")
+        if (!req.body.email) throw res.status(400).send("email is required")
+        if (!req.body.address) throw res.status(400).send("address is required")
+        if (!req.body.name) throw res.status(400).send("name is required")
+        if (!req.body.address.street) throw res.status(400).send("Street is required")
+        if (!req.body.address.number) throw res.status(400).send("number is required")
+        if (!req.body.address.city) throw res.status(400).send("city is required")
+        if (!req.body.address.location) throw res.status(400).send("location is required")
+        if (!req.body.address.municipality) throw res.status(400).send("municipality is required")
         let { email, name } = req.body
         let { street, number, city, location, municipality } = req.body.address
-        let address:Address = {
+        let address: Address = {
             street,
             number,
             city,
             location,
             municipality
         }
-        let response:any = await this.ingenioDao.getIngenioByEmail(email);
-        if(response.length) {throw res.status(400).send({msg:"Email is register"});}
+        let response: any = await this.ingenioDao.getIngenioByEmail(email);
+        if (response.length) { throw res.status(400).send({ msg: "Email is register" }); }
 
         await this.addressDao.CreateAddress(address);
-        let response1:any = await this.addressDao.getAddresByAttributes(address)
-        let addressid:number = parseInt(response1[0].id);
+        let response1: any = await this.addressDao.getAddresByAttributes(address)
+        let addressid: number = parseInt(response1[0].id);
         logger.info(addressid)
-        let ingenio:Ingenio = {
+        let ingenio: Ingenio = {
             addressid,
             email,
             name
@@ -67,23 +67,23 @@ export default class IngenioController{
         logger.debug('CONTROLLER: method createeIngenio Ending');
     }
 
-    async deleteIngenioById(req:Request, res:Response){
+    async deleteIngenioById(req: Request, res: Response) {
         logger.info('CONTROLLER: method createeIngenio Starting');
-        if(!req.params.ingenioId) throw res.status(400).send({msg: 'ingenioId is required'});
-        let ingenioId:any = req.params.ingenioId;
+        if (!req.params.ingenioId) throw res.status(400).send({ msg: 'ingenioId is required' });
+        let ingenioId: any = req.params.ingenioId;
         await this.ingenioDao.deleteIngeniosById(ingenioId);
         res.status(200).send({})
         logger.debug('CONTROLLER: method createeIngenio Ending');
     }
 
-    async sendEmail(req:any,res:Response){
+    async sendEmail(req: any, res: Response) {
         logger.info('CONTROLLER: method sendEmail Starting');
-        if(!req.params.orderId) throw res.status(400).send("orderId is required");
-        if(!req.body.ingenioId) throw res.status(400).send("ingenioId is required");
+        if (!req.params.orderId) throw res.status(400).send("orderId is required");
+        if (!req.body.ingenioId) throw res.status(400).send("ingenioId is required");
         let orderId = req.params.orderId;
         let ingenioId = req.body.ingenioId;
-        let dataOrder:any = await this.orderDao.orderById(orderId);
-        if(!dataOrder[0]) throw res.status(400).send('{ "msg":"orderId not found"}');
+        let dataOrder: any = await this.orderDao.orderById(orderId);
+        if (!dataOrder[0]) throw res.status(400).send('{ "msg":"orderId not found"}');
         let response = parseInt(dataOrder[0].addressid)
         logger.info(response)
         let address: any = await this.addressDao.getAddressById(response);
@@ -116,48 +116,57 @@ export default class IngenioController{
             status: `${dataOrder[0].status}`,
             subOrders: sub
         }
-        if(order.status!="PENDING"){
-        let ingenio:any = await this.ingenioDao.getIngenioById(ingenioId);
-        let email = ingenio[0].email;
-        logger.info(order)
-        let report =await this.pdfHelper.getRemissionDocument(order);
-        pdf.create(report,{ format: 'Letter',border: {
-            top: "1in",            // default is 0, units: mm, cm, in, px
-            right: "0in",
-            bottom: "2in",
-            left: "2cm"
-          } }).toStream(async (err,stream)=>{
-             logger.info("Email:",email);
-             await this.nodemailerHelper.sendMail({ content: stream, filename: `${orderId}.pdf`},email);
-             res.send({msg:'OK'});
-        })
-    }else{
-        res.send({msg: "La carga aun no se ha completado"});
-    }
-    logger.debug('CONTROLLER: method sendEmail Ending');
+        if (order.status != "PENDING") {
+            let ingenio: any = await this.ingenioDao.getIngenioById(ingenioId);
+            let email = ingenio[0].email;
+            logger.info(order)
+            let report = await this.pdfHelper.getRemissionDocument(order);
+            pdf.create(report, {
+                format: 'Letter', border: {
+                    top: "1in",            // default is 0, units: mm, cm, in, px
+                    right: "0in",
+                    bottom: "2in",
+                    left: "2cm"
+                }
+            }).toStream(async (err, stream) => {
+                logger.info("Email:", email);
+                await this.nodemailerHelper.sendMail({ content: stream, filename: `${orderId}.pdf` }, email);
+                res.send({ msg: 'OK' });
+            })
+        } else {
+            res.send({ msg: "La carga aun no se ha completado" });
+        }
+        logger.debug('CONTROLLER: method sendEmail Ending');
     }
 
-    async getIngenios(req:Request, res:Response){
+    async getIngenios(req: Request, res: Response) {
         logger.info('CONTROLLER: Method getIngenios Stratting');
-        let ingenios:any = await this.ingenioDao.getAllIngenios();
-        let structureIngenios:any=[];
+        let num_page: number = req.query.numPage;
+        let total_page: number = req.query.totalPage;
+        let discard: number = num_page * total_page;
+        if (!num_page) throw res.status(400).send(`numPage is required`);
+        if (!total_page) throw res.status(400).send(`totalPage is required`);
 
-        for(let ingenio of ingenios){
-            let addressid:any= ingenio.addressid;
-            let dataAddress:any = await this.addressDao.getAddressById(addressid);
-            let structureaddress:any={};
-            structureaddress={
+        let ingenios: any = await this.ingenioDao.getAllIngenios(discard, total_page);
+        let structureIngenios: any = [];
+
+        for (let ingenio of ingenios) {
+            logger.info(`structureIngenios: ${structureIngenios.length}`);
+            let addressid: any = ingenio.addressid;
+            let dataAddress: any = await this.addressDao.getAddressById(addressid);
+            let structureaddress: any = {};
+            structureaddress = {
                 calle: JSON.parse(dataAddress[0].calle),
                 numero: JSON.parse(dataAddress[0].numero),
                 ciudad: `${dataAddress[0].ciudad}`,
                 localidad: `${dataAddress[0].localidad}`,
                 municipio: `${dataAddress[0].municipio}`
-            } 
+            }
             structureIngenios.push({
-                id:`${ingenio.id}`,
-                email:`${ingenio.email}`,
-                name:`${ingenio.name}`,
-                address:structureaddress
+                id: `${ingenio.id}`,
+                email: `${ingenio.email}`,
+                name: `${ingenio.name}`,
+                address: structureaddress
             })
         }
         logger.debug('CONTROLLER: Method getIngenios Ending');
